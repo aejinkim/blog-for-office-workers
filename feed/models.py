b138bdf0be96457da34d4e19691746dfcce2a081
aejinkim <arias0410@aejinkims-MacBook-Pro.local>
2017-01-24 01:07:51 +0900

    Modified detail,navbar,admin

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    DEVELOPMENT = "dv" # 대문자는 변하지 않음을 의미함
+    PERSONAL = "ps"
+    CATEGORY_CHOICES = (
+        (DEVELOPMENT, "development"),
+        (PERSONAL, "personal"),
+    )
+
+    title = models.CharField(max_length=200)
+    contents = models.TextField()
+    category = models.CharField(
+        max_length=2,
+        choices=CATEGORY_CHOICES,
+        default=DEVELOPMENT,
+    )
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    username = models.CharField(max_length=50)
+    contents = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "{}에 댓글 : {}".format(self.article.title, self.contents)
+
+class HashTag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    pass
+
+def detatil(request):
+    pass
+
+# def about(request):
+#     pass
